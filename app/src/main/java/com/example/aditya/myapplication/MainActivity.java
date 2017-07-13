d1a6a6b4516ddf5f2e7fa666d337fd2c5286e3ea
Aditya <aditya21196@gmail.com>
2017-07-13 16:33:03 +0530

    long scrollable views

--- app/src/main/java/com/example/aditya/myapplication/MainActivity.java
+++ app/src/main/java/com/example/aditya/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import com.github.paolorotolo.expandableheightlistview.ExpandableHeightListView;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -18,11 +20,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         data d1=new data("COE 1","COE 2","1st august","1:30");
         data[] dat={
-                d1,d1
+                d1,d1,d1,d1,d1,d1
         };
-        ListView listView = (ListView) findViewById(R.id.lv);
+        ExpandableHeightListView listView = (ExpandableHeightListView) findViewById(R.id.lv);
         adapter myAdapter = new adapter(this, Arrays.asList(dat));
+        listView.setExpanded(true);
 
         listView.setAdapter(myAdapter);
+        dataf2 df1=new dataf2("ICE 1","ICE 2","2nd august","2:0");
+        dataf2[] dataf2={
+                df1,df1,df1,df1,df1,df1
+        };
+        ExpandableHeightListView listView2 = (ExpandableHeightListView) findViewById(R.id.lv2);
+        adapter2 myAdapter2 = new adapter2(this, Arrays.asList(dataf2));
+        listView2.setExpanded(true);
+        listView2.setAdapter(myAdapter2);
     }
 }
---
+++ app/src/main/java/com/example/aditya/myapplication/dataf2.java
@@ -0,0 +1,21 @@
+package com.example.aditya.myapplication;
+
+/**
+ * Created by aditya on 13/7/17.
+ */
+
+public class dataf2 {
+    String team1;
+    String team2;
+    String date;
+    String score;
+
+    public dataf2 (String t1, String t2, String d, String s)
+    {
+        this.team1 = t1;
+        this.team2 = t2;
+        this.date = d;
+        this.score=s;
+    }
+
+}
